5f3b1b4d8c3a629dd7aa3e5e021ea6536e118888
Mattis Kancans Envall <mattiskan@gmail.com>
2022-10-02 15:52:58 +0200

    csrf doesnt make sense for APIs

--- jbl_chat/jbl_chat/settings.py
+++ jbl_chat/jbl_chat/settings.py
@@ -45,7 +45,10 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # csrf doesn't make sense for an API.
+    # I would actually prefer to only disable this only for the chat app
+    # and not globally, but leaving as a todo for now. (would not ship like this).
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
