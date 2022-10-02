89e72708538259d6a6b0d35d51efe57cb6f9cf00
Mattis Kancans Envall <mattiskan@gmail.com>
2022-10-02 13:12:05 +0200

    stub out API

--- jbl_chat/chat/urls.py
+++ jbl_chat/chat/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('users', views.get_users, name='get_users'),
+    path('conversation', views.get_conversation, name='get_conversation'),
+    path('message', views.post_message, name='post_message'),    
 ]
--- jbl_chat/chat/views.py
+++ jbl_chat/chat/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
-    return HttpResponse("Tjena världen, från Mattis")
+    return JsonResponse("Tjena världen, från Mattis")
+
+
+
+user = {
+    'id': 3,
+    'name': 'Ben'
+}
+
+message = {
+    'id': 123,
+    'sent_time': '2020-03-20T14:28:23.382748',
+    'text': 'Hi there, how are you?'
+}
+
+
+def get_users(request):
+    return JsonResponse({
+        'users': [user] * 3,
+    })
+
+def get_conversation(request):
+    return JsonResponse({
+        'recipient': user,
+        'conversation': [message] * 3,
+    })
+
+def post_message(request):
+    return JsonResponse({})
